8e41059501edaefd14c8d80ecc7121f2e42e9703
RuslanKhasakhanov <khasakhanov22@gmail.com>
2026-02-19 15:22:46 +0300

    19/02/25

---
+++ Zadachya6.py
@@ -0,0 +1,20 @@
+class Triangle:
+    def __init__(self,a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def perimeter(self):
+        return self.a + self.b + self.c
+    
+class EquilateralTriangle(Triangle):
+    def __init__(self, side):
+        self.a = self.b = self.c = side
+
+print(issubclass(EquilateralTriangle, Triangle))
+
+
+triangle1 = Triangle(3, 4, 5) 
+triangle2 = EquilateralTriangle(3) 
+print(triangle1.perimeter()) 
+print(triangle2.perimeter())
