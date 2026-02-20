76714cde9015d04f3f5497c8217e3c9a98f1f6e1
RuslanKhasakhanov <khasakhanov22@gmail.com>
2026-02-20 11:45:48 +0300

    20.02.26

--- Zadachya7.py
+++ Zadachya7.py
@@ -1,22 +1,21 @@
 class Summator:
+    power = 1   
     def total(self, n):
-        return sum(i for i in range(1, n + 1))
-
-
-class SquareSummator:
-    def total(self, n):
-        return sum(i ** 2 for i in range(1, n + 1))
-
-class QubeSummator:
-    def total(self, n):
-        return sum(i ** 3 for i in range(1, n + 1))
-
-class CustomSummator:
-    def __init__(self, m):
-        self.m = m
+        return sum(i ** self.power for i in range(1, n + 1))
+        
+class SquareSummator(Summator):
+    power = 2
     
-    def total(self, n):
-        return sum(i ** self.m for i in range(1, n + 1))
+class QubeSummator(Summator):
+    power = 3
+   
+class CustomSummator(Summator):
+    def __init__(self, m):
+        super().__init__()
+        self.power = m
+
+print(issubclass(SquareSummator, Summator)) 
+print(issubclass(QubeSummator, Summator))
 
 summator1 = Summator()
 summator2 = SquareSummator()
@@ -24,4 +23,4 @@ summator3 = QubeSummator()
 
 print(summator1.total(3))
 print(summator2.total(3))
-print(summator3.total(3))    
+print(summator3.total(3))
